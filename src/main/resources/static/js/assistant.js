33fa8363910a502b1b4af8b4afeedea09d28067e
Lili <51219138+devlilibel@users.noreply.github.com>
2023-05-04 17:07:39 -0500

    Update assistant.js

--- src/main/resources/static/js/assistant.js
+++ src/main/resources/static/js/assistant.js
@@ -39,7 +39,7 @@ if (count <= 0){
 EasySpeech.detect();
 await EasySpeech.init();
 console.log(EasySpeech.voices());
-const voice = EasySpeech.voices()[233];
+const voice = EasySpeech.voices()[240];
 var text = null;
 /*const synth = window.speechSynthesis;
 const tts = new SpeechSynthesisUtterance();
